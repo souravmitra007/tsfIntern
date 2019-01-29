d6426883df26eaa1f9182d65534cc42462da875a
Sourav Mitra <you@example.com>
2019-01-29 23:03:45 +0530

    Project File

---
+++ tsfIntern/src/main/java/com/tsfIntern/TsfInternApplication.java
@@ -0,0 +1,14 @@
+package com.tsfIntern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsfInternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TsfInternApplication.class, args);
+	}
+
+}
+
